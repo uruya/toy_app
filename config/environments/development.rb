b8deca75c3dcb96b2fc821bdffc34426deb5fe1f
uruya <1ayawolf1@gmail.com>
2021-08-08 06:49:32 +0000

    Finish toy app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
@@ -59,4 +61,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Cloud9 への接続を許可する
+  config.hosts.clear
 end
